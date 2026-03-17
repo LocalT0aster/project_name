5e0971059ac64ffb4fec777489ca6a826bf12f9b
LocalT0aster <90502400+LocalT0aster@users.noreply.github.com>
2026-03-17 14:58:55 +0300

    mv: Refactor StringNames into const

--- objects/mechanic_drager.gd
+++ objects/mechanic_drager.gd
@@ -2,6 +2,6 @@ extends StaticBody2D
 
 var enabled := false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if enabled:
 		global_position = get_global_mouse_position()
