7e84bce26929865e424d5473c1c990c6b6d9a726
LocalT0aster <90502400+LocalT0aster@users.noreply.github.com>
2026-03-30 10:02:18 +0300

    mechanic_tree

--- mechanics/mechanics_tree.gd
+++ mechanics/mechanics_tree.gd
@@ -1,7 +1,22 @@
 class_name MechanicsTree
 extends Node
 
-@export var slots: Array[PackedScene]
+@export var default_slots: Array[PackedScene]
+@export var loaded: Array[FSM]
 
 func _ready() -> void:
 	MechanicManager.object_tree[get_parent().name] = self
+	for s in default_slots:
+		var fsm = s.instantiate() as FSM
+		loaded.append(fsm)
+		add_child(fsm)
+
+func set_slot(index: int, scene: PackedScene) -> void:
+	if index >= default_slots.size() or index < 0:
+		printerr("MechanicsTree: got out of bounds index")
+	loaded[index].queue_free()
+	if not scene:
+		scene = default_slots[index]
+	var fsm = scene.instantiate() as FSM
+	loaded.append(fsm)
+	add_child(fsm)
