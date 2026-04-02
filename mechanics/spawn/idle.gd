e3294058cc7ff9b9feed87fa777ff4a4b27d7d2f
corndev <128768882+nonamecorn@users.noreply.github.com>
2026-04-03 00:29:28 +0300

    FSM and spawn

--- mechanics/spawn/idle.gd
+++ mechanics/spawn/idle.gd
@@ -2,6 +2,10 @@ extends State
 
 const SPAWN_ACTION: StringName = &"left_mouse_click"
 
-func physics_update(_delta : float) -> void:
+func unhandled_event(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed(SPAWN_ACTION):
 		transition.emit(self, "spawn")
+
+#func physics_update(_delta : float) -> void:
+	#if Input.is_action_just_pressed(SPAWN_ACTION):
+		#transition.emit(self, "spawn")
--- other/State.gd
+++ other/State.gd
@@ -12,3 +12,7 @@ func update(_delta : float) -> void:
 	pass
 func physics_update(_delta : float) -> void:
 	pass
+func unhandled_event(_event: InputEvent) -> void:
+	pass
+func event(_event: InputEvent) -> void:
+	pass
