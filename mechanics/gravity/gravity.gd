b4720362bb840877150ad0e4e51fa548270a37ac
LocalT0aster <90502400+LocalT0aster@users.noreply.github.com>
2026-04-19 21:47:00 +0300

    Customizable gravity angle

--- mechanics/gravity/gravity.gd
+++ mechanics/gravity/gravity.gd
@@ -1,8 +1,11 @@
 extends CharacterFSM
 
+## Multiples of PI
+@export var angle_pi_rad: float = 1.0
+
 func _ready() -> void:
 	super ()
-	character.rotation = PI
+	character.rotation = PI * angle_pi_rad
 
 func _exit_tree() -> void:
 	character.rotation = 0.0
