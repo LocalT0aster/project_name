f2fcf4c9655fb43474bcee63179cfb5338216ff1
LocalT0aster <90502400+LocalT0aster@users.noreply.github.com>
2026-05-04 00:26:10 +0300

    fix: golem orientation

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -3,4 +3,8 @@ extends CharacterBody2D
 func hurt(_a, _b):
 	queue_free()
 
-@export var sprite : Sprite2D
+@export var sprite: Sprite2D
+@export var look_left_at_start: bool = false
+
+func _ready() -> void:
+	sprite.scale.x = -abs(sprite.scale.x) if look_left_at_start else abs(sprite.scale.x)
