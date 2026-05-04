a6989535fd5fe09bb60ebfb500128768445f0d6e
corndev <128768882+nonamecorn@users.noreply.github.com>
2026-05-04 04:02:02 +0300

    work

---
+++ objects/interactor.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@export var enabled : bool = true
+
+func _input(event: InputEvent) -> void:
+	if !enabled or get_overlapping_areas().size() == 0: return
+	if Input.is_action_pressed("interact"):
+		print("ses")
+		var dil = get_overlapping_areas()[0].dialog
+		if Dialogic.current_timeline != null:
+			Dialogic.end_timeline()
+		Dialogic.start(dil)
+	
