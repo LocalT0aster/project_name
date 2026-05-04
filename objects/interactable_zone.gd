6d7900b8140ef4e3b086d5663e0e1f92bd5eb82a
corndev <128768882+nonamecorn@users.noreply.github.com>
2026-05-04 04:21:49 +0300

    interactibles

--- objects/interactable_zone.gd
+++ objects/interactable_zone.gd
@@ -1,3 +1,12 @@
 extends Area2D
 
 @export var dialog : String = 'Default'
+@onready var light: PointLight2D = $PointLight2D
+
+
+func _on_area_entered(area: Area2D) -> void:
+	light.enabled = true
+
+
+func _on_area_exited(area: Area2D) -> void:
+	light.enabled = false
