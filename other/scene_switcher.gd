86fb5f20b76e7609991be3101bacf77cdbbad780
LocalT0aster <90502400+LocalT0aster@users.noreply.github.com>
2026-05-18 04:33:49 +0300

    scene_switcher? Is it even used?

--- other/scene_switcher.gd
+++ other/scene_switcher.gd
@@ -2,14 +2,16 @@ extends CanvasLayer
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var scene_path : String = ""
+@export_file("*.tscn") var scene_path: String = ""
 
-func switch_scene(path):
+const ANIMATION_NAME: StringName = &"fade"
+
+func switch_scene(path: String):
 	scene_path = path
-	animation_player.play("fade")
+	animation_player.play(ANIMATION_NAME)
 
 func finish_switch():
 	if scene_path == "": return
 	get_tree().change_scene_to_file(scene_path)
 	scene_path = ""
-	animation_player.play_backwards("fade")
+	animation_player.play_backwards(ANIMATION_NAME)
